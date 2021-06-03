81dbcc60344a0cba639a460002dd27e4628443c2
Eric Potter <eric.b.potter@gmail.com>
2021-06-02 22:12:47 -0400

    Add project files.

---
+++ PomodoroProgressBar/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace PomodoroProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblCompleted = new System.Windows.Forms.Label();
+            this.lblStatusText = new System.Windows.Forms.Label();
+            this.lblCompletedCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(150, 46);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(12, 73);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(150, 46);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(186, 19);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(83, 32);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "Status:";
+            // 
+            // lblCompleted
+            // 
+            this.lblCompleted.AutoSize = true;
+            this.lblCompleted.Location = new System.Drawing.Point(186, 80);
+            this.lblCompleted.Name = "lblCompleted";
+            this.lblCompleted.Size = new System.Drawing.Size(137, 32);
+            this.lblCompleted.TabIndex = 3;
+            this.lblCompleted.Text = "Completed:";
+            // 
+            // lblStatusText
+            // 
+            this.lblStatusText.AutoSize = true;
+            this.lblStatusText.Location = new System.Drawing.Point(362, 19);
+            this.lblStatusText.Name = "lblStatusText";
+            this.lblStatusText.Size = new System.Drawing.Size(0, 32);
+            this.lblStatusText.TabIndex = 4;
+            // 
+            // lblCompletedCount
+            // 
+            this.lblCompletedCount.AutoSize = true;
+            this.lblCompletedCount.Location = new System.Drawing.Point(362, 80);
+            this.lblCompletedCount.Name = "lblCompletedCount";
+            this.lblCompletedCount.Size = new System.Drawing.Size(27, 32);
+            this.lblCompletedCount.TabIndex = 5;
+            this.lblCompletedCount.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 143);
+            this.Controls.Add(this.lblCompletedCount);
+            this.Controls.Add(this.lblStatusText);
+            this.Controls.Add(this.lblCompleted);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "PomodoroProgress";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblCompleted;
+        private System.Windows.Forms.Label lblStatusText;
+        private System.Windows.Forms.Label lblCompletedCount;
+    }
+}
+
---
+++ PomodoroProgressBar/Form1.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.WindowsAPICodePack.Taskbar;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PomodoroProgressBar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            _pomodoroTimer.Interval = TIMER_INTERVAL;
+            _pomodoroTimer.Tick += _pomodoroTimer_Tick;
+        }
+
+
+        private const int TIMER_INTERVAL = 10_000;
+        private const int TIMER_INTERVAL_IN_TICKS = 10_000 * (int)TimeSpan.TicksPerMillisecond;
+        private const long WORK_INTERVAL = 25 * TimeSpan.TicksPerMinute;
+        private const long NONWARNING_WORK_INTERVAL = 20 * TimeSpan.TicksPerMinute;
+        private long _startTime;
+        private Timer _pomodoroTimer = new Timer();
+        public long Progress { get; set; } = 0;
+        public TaskbarProgressBarState State { get; set; } = TaskbarProgressBarState.NoProgress;
+        private int _completed = 0;
+
+        private void _pomodoroTimer_Tick(object sender, EventArgs e)
+        {
+            var currentTime = DateTime.Now.Ticks;
+            var elapsedTime = currentTime - _startTime;
+
+            if(elapsedTime >= WORK_INTERVAL)
+            {
+                State = TaskbarProgressBarState.Error;
+                _pomodoroTimer.Stop();
+                _completed++;
+                lblCompletedCount.Text = _completed.ToString();
+                lblStatusText.Text = "Finished";
+            }
+            else if (elapsedTime >= NONWARNING_WORK_INTERVAL)
+            {
+                State = TaskbarProgressBarState.Paused;
+            }
+            else
+            {
+                State = TaskbarProgressBarState.Normal;
+            }
+
+            Progress = elapsedTime;
+            UpdateTaskbar();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            State = TaskbarProgressBarState.Normal;
+            Progress = 0;
+
+            _startTime = DateTime.Now.Ticks;
+            _pomodoroTimer.Start();
+
+            UpdateTaskbar();
+
+            lblStatusText.Text = "Running";
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            State = TaskbarProgressBarState.Normal;
+            UpdateTaskbar();
+        }
+
+        private void UpdateTaskbar()
+        {
+            var taskbar = Microsoft.WindowsAPICodePack.Taskbar.TaskbarManager.Instance;
+            taskbar.SetProgressState(State);
+            taskbar.SetProgressValue((int)(Progress/ TIMER_INTERVAL_IN_TICKS), (int)(WORK_INTERVAL/ TIMER_INTERVAL_IN_TICKS));
+        }
+    }
+}
